7538d9048d4d54938160f6eb39bd041b8397d2fd
Jack <jcm2022@icloud.com>
2024-12-21 23:32:55 -0500

    improved readability, logging etc

--- main.py
+++ main.py
@@ -1,51 +1,34 @@
+import json
+import sys
 from tm import OneTapeTuringMachine
+from transitions import transitions_equal01s, transitions_identical01s
 
-transitions = {
-        'q_1' : {
-            '0' : ('q_2', 'x', 'R'),
-            '1' : ('q_1', '1', 'R'),
-            'x' : ('q_1', 'x', 'R'),
-            '$' : ('q_1', '$', 'R'),
-            '_' : ('q_5', '_', 'L')
-                   },
-        'q_2' : {
-            '0' : ('q_2', '0', 'L'),
-            '1' : ('q_2', '1', 'L'),
-            'x' : ('q_2', 'x', 'L'),
-            '$' : ('q_3', '$', 'R')
-            },
-        'q_3' : {
-            '0' : ('q_3', '0', 'R'),
-            '1' : ('q_4', 'x', 'L'),
-            'x' : ('q_3', 'x', 'R'),
-            },
-        'q_4' : {
-            '0' : ('q_4', '0', 'L'),
-            '1' : ('q_4', '1', 'L'),
-            'x' : ('q_4', 'x', 'L'),
-            '$' : ('q_1', '$', 'R')
-            },
-        'q_5' : {
-            '0' : ('q_5', '0', 'L'),
-            '1' : ('q_5', '1', 'L'),
-            'x' : ('q_5', 'x', 'L'),
-            '$' : ('q_6', '$', 'R'),
-        },
-        'q_6' : {
-            '0' : ('q_6', '0', 'R'),
-            '1' : ('q_reject', '1', 'R'),
-            'x' : ('q_6', 'x', 'R'),
-            '_' : ('q_accept', '_', 'R')
-            }
-        }
+machine_type = 'test_identical'
 
-input_string = "010100011101"
+if machine_type == 'test_identical':
+    input_string = '1000001#1000001'
+    transitions = transitions_identical01s
+
+elif machine_type == 'test_equal_amount':
+    input_string = '010100011101'
+    transitions = transitions_equal01s
+else:
+    print("invalid machine type selected. Options are:")
+    print("test_identical: tests if two strings comprised of 0s and 1s are identical")
+    print("test_equal_amount: tests if a string has the same amount of 0s as 1s")
+    sys.exit(1)
+
 # process input string by prepending '$' and appending '_' (blank symbol)
-TM = OneTapeTuringMachine(['$'] + list(input_string) + ['_'], transitions)
+input_string = ['$'] + list(input_string) + ['_'] 
+
+# construct the turing machine
+TM = OneTapeTuringMachine(input_string, transitions)
 TM.display()
 
+
+# run the Turing Machine
 num_transitions = 0
-while(TM.state != 'q_accept' and TM.state != 'q_reject'):
+while (TM.state not in ('q_accept', 'q_reject')):
     current_symbol = TM.read_tape()
     if TM.state in TM.transitions and current_symbol in TM.transitions[TM.state]:
         (next_state, new_tape_symbol, direction) = TM.transitions[TM.state][current_symbol]
@@ -53,7 +36,9 @@ while(TM.state != 'q_accept' and TM.state != 'q_reject'):
         TM.state = next_state
         TM.move_head(direction)
 
-    else: # all undefined transitions go to qreject
+    else: # all undefined transitions go to q_reject
+        print(TM.state in TM.transitions)
+        print(current_symbol in TM.transitions[TM.state])
         TM.state = 'q_reject'
 
     num_transitions += 1
